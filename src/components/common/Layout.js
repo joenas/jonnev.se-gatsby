8b6997d6e56fc41db98032111667b9d97898700d
joenas <jon@jonnev.se>
2022-05-06 23:44:58 +0200

    Add Fathom tracking snippet

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -28,6 +28,19 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             <Helmet>
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
+                <script>{`
+                    (function(f, a, t, h, o, m){
+                        a[h]=a[h]||function(){
+                            (a[h].q=a[h].q||[]).push(arguments)
+                        };
+                        o=f.createElement('script'),
+                        m=f.getElementsByTagName('script')[0];
+                        o.async=1; o.src=t; o.id='fathom-script';
+                        m.parentNode.insertBefore(o,m)
+                    })(document, window, '//fathom.jonnev.dev/tracker.js', 'fathom');
+                    fathom('set', 'siteId', 'XTKLG');
+                    fathom('trackPageview');
+                `}</script>
                 <body className={bodyClass} />
             </Helmet>
 
